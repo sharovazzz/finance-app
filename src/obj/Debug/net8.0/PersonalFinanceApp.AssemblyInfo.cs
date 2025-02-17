ce60bfdab84115400d45dbdb6d71fff7e9913e26
KotovaAnastasiya <nastakotova81240@gmail.com>
2025-02-17 17:33:16 +0700

    #19 Fixed the repository structure. Closes #19

--- src/obj/Debug/net8.0/PersonalFinanceApp.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/PersonalFinanceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fce8f6242d12e0da4857997edb8dc1aed719546")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
