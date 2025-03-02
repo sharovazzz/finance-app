b85f4ebc66f75f203565ad22f84929fbb3d1a23f
KotovaAnastasiya <nastakotova81240@gmail.com>
2025-03-03 00:28:37 +0700

    #13 Add tests on UpdateUser.

--- PersonalFinanceApp.Tests/Service/UserServiceTests.cs
+++ PersonalFinanceApp.Tests/Service/UserServiceTests.cs
@@ -134,5 +134,187 @@ namespace PersonalFinanceApp.Tests
 
             Assert.Equal("nan@gmail.com", result.Email);
         }
+
+        [Fact]
+        public void UpdateUser_NotFoundUser_Exeption()
+        {
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "na@gmail.com",
+                Phone = ""
+            };
+
+            Action act = () => _userService.UpdateUser(2, updateUserDto);
+
+            KeyNotFoundException exeption = Assert.Throws<KeyNotFoundException>(act);
+            Assert.Equal("User not found", exeption.Message);
+        }
+
+        [Fact]
+        public void UpdateUser_EmptyEmailAndPhone_Exeption()
+        {
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "na@gmail.com",
+                Phone = "79961110000"
+            };
+            _userRepository.CreateUser(createUserDto);
+            
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "",
+                Phone = ""
+            };
+
+            Action act = () => _userService.UpdateUser(1, updateUserDto);
+
+            ArgumentException exeption = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("Either email or phone must be provided.", exeption.Message);
+        }
+
+        [Fact]
+        public void UpdateUser_IncorrectPhone_Exeption()
+        {
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "",
+                Phone = "79961110000"
+            };
+            _userRepository.CreateUser(createUserDto);
+
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "",
+                Phone = "123"
+            };
+
+            Action act = () => _userService.UpdateUser(1, updateUserDto);
+
+            ArgumentException exeption = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("Incorrect phone number.", exeption.Message);
+        }
+
+        [Fact]
+        public void UpdateUser_UserWithThisPhoneAlredyExists_Exeption()
+        {
+            var createAnyUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "",
+                Phone = "79961110000"
+            };
+            _userRepository.CreateUser(createAnyUserDto);
+
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "na@gmail.com",
+                Phone = ""
+            };
+            _userRepository.CreateUser(createUserDto);
+
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "na@gmail.com",
+                Phone = "79961110000"
+            };
+
+            Action act = () => _userService.UpdateUser(2, updateUserDto);
+
+            ArgumentException exeption = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("User with this email or phone already exists.", exeption.Message);
+        }
+
+        [Fact]
+        public void UpdateUser_ValidData_NotNull()
+        {
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "na@gmail.com",
+                Phone = ""
+            };
+            _userRepository.CreateUser(createUserDto);
+
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "na@gmail.com",
+                Phone = "89961110000"
+            };
+
+            _userService.UpdateUser(1, updateUserDto);
+            var updatedUser = _userRepository.GetUser(1);
+
+            Assert.NotNull(updatedUser);
+        }
+
+        [Fact]
+        public void UpdateUser_NotFormattedPhone_FormattedPhone()
+        {
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "na@gmail.com",
+                Phone = ""
+            };
+            _userRepository.CreateUser(createUserDto);
+
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "na@gmail.com",
+                Phone = "89961110000"
+            };
+
+            _userService.UpdateUser(1, updateUserDto);
+            var updatedUser = _userRepository.GetUser(1);
+
+            Assert.Equal("79961110000", updatedUser.Phone);
+        }
+
+        [Fact]
+        public void UpdateUser_NotFormattedEmail_FormattedEmail()
+        {
+            var createUserDto = new UserDto
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Email = "",
+                Phone = "79961110000"
+            };
+            _userRepository.CreateUser(createUserDto);
+
+            var updateUserDto = new UserDto
+            {
+                FirstName = "Name",
+                LastName = "Name",
+                Email = "Na@gmail.com",
+                Phone = "79961110000"
+            };
+
+            _userService.UpdateUser(1, updateUserDto);
+            var updatedUser = _userRepository.GetUser(1);
+
+            Assert.Equal("na@gmail.com", updatedUser.Email);
+        }
     }
 }
